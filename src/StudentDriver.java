052b11330f70cae9859c1f8b384c6c93d900b059
venothx7 <venothx7@hotmail.com>
2016-09-16 10:16:14 -0400

    submitted version

--- src/StudentDriver.java
+++ src/StudentDriver.java
@@ -1,50 +1,50 @@
-import javax.swing.*;
-import java.awt.*;
+import java.util.ArrayList;
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import javax.swing.*;
 import java.io.FileInputStream;
-
+import java.io.File;
+import java.io.FileWriter;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-
 
 
 public class StudentDriver {
-    static String fileName = "313data.txt";
-    static ArrayList<Student> studentList = new ArrayList<>();
+    private static final String FILE_NAME = "313data.txt";
+    private static ArrayList<Student> studentList;
+
 
     public static void main(String[] args) {
 
 
         LoadData();
         AddStudent();
-        Display();
-
-
+        //Display(studentList);
+        StoreData();
 
     }
 
+
+    /**
+     * Loads the data from 313data.txt and adds it to studentList
+     */
     public static void LoadData() {
-
-
-
+        // create a ArrayList called courseList of object Course
+        studentList = new ArrayList<>();
+        ArrayList<Course> courseList = new ArrayList<>();
 
         try {
             BufferedReader file = new BufferedReader(
-                    new InputStreamReader(new FileInputStream(fileName)));
+                    new InputStreamReader(new FileInputStream(FILE_NAME)));
 
             String input = file.readLine();
             while (input != null) {
-                //System.out.println();
                 String[] temp = input.split(",");
                 //temp[0] holds String Last Name
                 //temp[1] holds String First name
                 //temp[2] holds String id
-                Student tempStudent = new Student(temp[0], temp[1], temp[2]); //read first line into Student
-
-                // create a ArrayList called courseList of object Course
-                ArrayList<Course> courseList = new ArrayList<>();
+                Student tempStudent = new Student(temp[0], temp[1], temp[2]);
 
                 input = file.readLine();
 
@@ -63,14 +63,12 @@ public class StudentDriver {
 
                 temp = input.split(",");
                 // Set the totalCredits and gpa into tempStudent
-                // tempStudent = new Student(Double.parseDouble(temp[0]),Double.parseDouble(temp[1]));// also doesnt work, sets gpa but not names
-                //tempStudent(Double.parseDouble(temp[0]),Double.parseDouble(temp[1])); // WHY CANT I DO THIS
                 tempStudent.setTotalCredits(Double.parseDouble(temp[0]));
                 tempStudent.setGpa(Double.parseDouble(temp[1]));
 
                 // adds the current courseList to the current tempStudent
                 tempStudent.setCourses(courseList);
-                // adds the current Student object to Student arraylist
+                // adds the current Student to studentList
                 studentList.add(tempStudent);
                 input = file.readLine();
 
@@ -83,33 +81,62 @@ public class StudentDriver {
             e.printStackTrace();
         }
 
+    }
 
+
+    public static void Display(ArrayList studentList) {
+
+        for (Object o : studentList) {
+            System.out.print(o.toString());
         }
 
+    }
 
 
-     public static void Display() {
-         for (Student s : studentList){
-             System.out.println(s);}
-     }
+    public static void AddStudent() {
+        int result = JOptionPane.showConfirmDialog(null,
+                "Would you like to add a Student", "Add Students", JOptionPane.YES_NO_OPTION);
+        //if they Click yes
+        while (result == 0) {
+            String input = JOptionPane.showInputDialog(null,
+                    "Enter Students Name(First Last, seperated with a space):",
+                    "Adding Student", JOptionPane.INFORMATION_MESSAGE);
+            String[] tempArray = input.split(" ");
+            Student tempStudent = new Student(tempArray[0], tempArray[1]);
+            studentList.add(tempStudent);
+            result = JOptionPane.showConfirmDialog(null,
+                    "Would you like to add another Student", "Add Students", JOptionPane.YES_NO_OPTION);
+        }
+    }
+
+    public static void StoreData() {
+
+        BufferedWriter bw = null;
+
+        try {
+            String strContent = "This example shows how to write string content to a files";
+            File file = new File("tester.txt");
+            // check if file exist, otherwise create the file before writing
+            if (!file.exists()) {
+                file.createNewFile();
+            }
+
+            FileWriter fw = new FileWriter(file);
+            bw = new BufferedWriter(fw);
+            for (Object o : studentList) {
+                bw.write(o.toString());
+            }
 
 
-
-     public static void AddStudent(){
-         int result= JOptionPane.showConfirmDialog(null,
-                 "Would you like to add a Student","Add Students", JOptionPane.YES_NO_OPTION);
-         //if they Click yes
-         while(result == 0){
-             String input = JOptionPane.showInputDialog(null,
-                     "Enter Students Name(First Last, seperated with a space):",
-                     "Adding Student", JOptionPane.INFORMATION_MESSAGE);
-             String[] tempArray= input.split(" ");
-             Student tempStudent = new Student(tempArray[0],tempArray[1]);
-             studentList.add(tempStudent);
-             result= JOptionPane.showConfirmDialog(null,
-                     "Would you like to add another Student","Add Students", JOptionPane.YES_NO_OPTION);
-         }
-     }
-
-
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (bw != null)
+                    bw.close();
+            } catch (Exception ex) {
+                System.out.println("Error in closing the BufferedWriter" + ex);
+            }
+        }
+    }
 }
